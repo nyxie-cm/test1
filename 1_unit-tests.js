09b53818cb248ac5e29363e42b4b11fb5053ee26
nyxie-cm <oleksandra.morgunova@gmail.com>
2024-10-08 23:15:47 +0300

    Update 1_unit-tests.js

--- 1_unit-tests.js
+++ 1_unit-tests.js
@@ -4,7 +4,7 @@ const assert = chai.assert
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
-    test('#isNulqcl, #isNotNull', function () {
+    test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
       assert.isNotNull(1, '1 is not null');
     });
